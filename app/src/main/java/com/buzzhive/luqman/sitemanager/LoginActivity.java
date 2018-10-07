893178b1d542b17d863dfa77488fd1d28a8e6ed6
luqmanCodes <37939871+luqmanCodes@users.noreply.github.com>
2018-10-07 22:29:53 +0530

    added some design changed

--- app/src/main/java/com/buzzhive/luqman/sitemanager/LoginActivity.java
+++ app/src/main/java/com/buzzhive/luqman/sitemanager/LoginActivity.java
@@ -194,12 +194,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return email.toLowerCase().contains("sm");
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
         return password.length() > 4;
     }
 
